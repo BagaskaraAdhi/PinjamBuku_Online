e0b990af8d375d59f5679f072d6c19b5cf3c120f
Bagaskara Adhi Pradana <85418809+BagaskaraAdhi@users.noreply.github.com>
2022-01-15 20:45:11 +0700

    Add files via upload

---
+++ db_pinjambuku.sql
@@ -0,0 +1,207 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: 15 Jan 2022 pada 13.09
+-- Versi Server: 10.1.19-MariaDB
+-- PHP Version: 5.6.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_pinjambuku`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `admin`
+--
+
+CREATE TABLE `admin` (
+  `user_id` int(2) NOT NULL,
+  `username` varchar(15) NOT NULL,
+  `password` varchar(15) NOT NULL,
+  `fullname` varchar(30) NOT NULL,
+  `gambar` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `admin`
+--
+
+INSERT INTO `admin` (`user_id`, `username`, `password`, `fullname`, `gambar`) VALUES
+(1, 'operator', 'operator', 'Irfan', 'gambar_admin/avatar5.png'),
+(2, 'admin', 'admin', 'Bagaskara adhi', 'gambar_admin/4.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `data_anggota`
+--
+
+CREATE TABLE `data_anggota` (
+  `id` int(4) NOT NULL,
+  `no_induk` varchar(5) NOT NULL,
+  `nama` varchar(150) NOT NULL,
+  `jk` varchar(2) NOT NULL,
+  `kelas` varchar(5) NOT NULL,
+  `ttl` varchar(100) NOT NULL,
+  `alamat` varchar(250) NOT NULL,
+  `foto` varchar(75) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `data_anggota`
+--
+
+INSERT INTO `data_anggota` (`id`, `no_induk`, `nama`, `jk`, `kelas`, `ttl`, `alamat`, `foto`) VALUES
+(5, '15905', 'RYAN SUPRIATNA', 'L', '9b', 'Cikarang, 9 Agustus 1995', 'Sukatani, Cikarang', 'gambar_anggota/ko caci.jpg'),
+(6, '15906', 'LATHIFAH', 'P', '9c', 'Bekasi, 26 Juli 1997', 'Cikarang Baru', 'gambar_anggota/1098.jpg'),
+(7, '15907', 'CANTIKA PUJIASTUTI', 'P', '9a', 'Karawang, 11 April 1998', 'Tambelang, Bekasi', 'gambar_anggota/user.jpg'),
+(8, '15908', 'SEBASTIAN HADI PRASETYO', 'L', '9a', 'Bekasi, 17 Agustus 1990', 'Tambelang, Bekasi', 'gambar_anggota/26115.jpg'),
+(9, '15909', 'RAMA AGUS SUPRIYADI', 'L', '9b', 'Bogor, 29 Juli 1994', 'Cikarang, Bekasi', 'gambar_anggota/l.jpg'),
+(12, '20150', 'ANTON SUGIANTO', 'L', '9b', 'Indramayu, 28 Oktober 1988', 'cikarang barat bekasi', 'gambar_anggota/2.jpg'),
+(13, '20150', 'DEDE RIZKI RAMADHAN', 'L', '9c', 'Bekasi, 30 November 1991', 'Pilar Barat, Cikarang, Bekasi', 'gambar_anggota/5.jpg'),
+(14, '00425', 'Udin', 'L', '9 B', 'Malang, 29 Februari 2000', 'Jl. Bayuwangi', 'gambar_anggota/7a7f12e11c3b01c410286dd9ca10a928.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `data_buku`
+--
+
+CREATE TABLE `data_buku` (
+  `id` int(5) NOT NULL,
+  `judul` varchar(250) NOT NULL,
+  `pengarang` varchar(250) NOT NULL,
+  `th_terbit` varchar(4) NOT NULL,
+  `penerbit` varchar(250) NOT NULL,
+  `isbn` varchar(25) NOT NULL,
+  `kategori` varchar(50) NOT NULL,
+  `kode_klas` varchar(20) NOT NULL,
+  `jumlah_buku` int(2) NOT NULL,
+  `lokasi` varchar(50) NOT NULL,
+  `asal` varchar(50) NOT NULL,
+  `jum_temp` int(4) NOT NULL,
+  `tgl_input` varchar(75) NOT NULL,
+  `gambar` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pengunjung`
+--
+
+CREATE TABLE `pengunjung` (
+  `id` int(6) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `jk` varchar(2) NOT NULL,
+  `kelas` varchar(17) NOT NULL,
+  `perlu1` varchar(15) NOT NULL,
+  `cari` varchar(255) NOT NULL,
+  `saran` varchar(255) NOT NULL,
+  `tgl_kunjung` date NOT NULL,
+  `jam_kunjung` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `pengunjung`
+--
+
+INSERT INTO `pengunjung` (`id`, `nama`, `jk`, `kelas`, `perlu1`, `cari`, `saran`, `tgl_kunjung`, `jam_kunjung`) VALUES
+(12, 'Udin', 'L', 'XI2', 'Pinjam Buku', 'Buku Coding', '', '2022-01-12', '06:48:03'),
+(13, 'Udin', 'L', 'XI3', 'Pinjam Buku', 'Buku Coding', '', '2022-01-12', '06:56:38');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `trans_pinjam`
+--
+
+CREATE TABLE `trans_pinjam` (
+  `id` int(5) NOT NULL,
+  `judul_buku` varchar(250) NOT NULL,
+  `id_peminjam` int(4) NOT NULL,
+  `nama_peminjam` varchar(100) NOT NULL,
+  `tgl_pinjam` varchar(15) NOT NULL,
+  `tgl_kembali` varchar(15) NOT NULL,
+  `status` varchar(10) NOT NULL,
+  `ket` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indexes for table `data_anggota`
+--
+ALTER TABLE `data_anggota`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `data_buku`
+--
+ALTER TABLE `data_buku`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pengunjung`
+--
+ALTER TABLE `pengunjung`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `trans_pinjam`
+--
+ALTER TABLE `trans_pinjam`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `user_id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `data_anggota`
+--
+ALTER TABLE `data_anggota`
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+--
+-- AUTO_INCREMENT for table `data_buku`
+--
+ALTER TABLE `data_buku`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `pengunjung`
+--
+ALTER TABLE `pengunjung`
+  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+--
+-- AUTO_INCREMENT for table `trans_pinjam`
+--
+ALTER TABLE `trans_pinjam`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
